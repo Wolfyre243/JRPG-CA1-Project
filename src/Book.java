b43191021c5d1562f0a883ad7f4ad1fa3de1ddf5
Wolfyre243 <wolfyreblueflare@gmail.com>
2025-06-09 16:41:23 +0800

    [FEATURE] code

---
+++ src/Book.java
@@ -0,0 +1,25 @@
+public class Book {
+    private String bookTitle; 
+    private String author; 
+    private String ISBN; 
+    private double price; 
+    private String category;
+    private boolean avaliableForLoan; 
+
+    public Book (String bookTitle, String author, String ISBN, double price, String category, boolean avaliableForLoan) { 
+        this.bookTitle = bookTitle; 
+        this.author = author;
+        this.ISBN = ISBN; 
+        this.price = price; 
+        this.category = category;
+        this.avaliableForLoan = avaliableForLoan;
+    }
+
+    public String getBookTitle() {
+        return bookTitle;
+    }
+
+    public void setBookTitle(String bookTitle) {
+        this.bookTitle = bookTitle;
+    }
+}
--- src/Student.java
+++ src/Student.java
@@ -8,13 +8,29 @@
  * @author wolfy
  */
 public class Student {
+    private int adminNumber;
     private String name;
 
-    public Student (String name) {
+    public Student (int adminNumber, String name) {
+        this.adminNumber = adminNumber;
         this.name = name;
     }
 
     public String getName() {
         return this.name;
     }
+
+    public void setName(String name) { 
+        this.name = name;
+    }
+
+    public int adminNumber() { 
+        return this.adminNumber;
+    }
+
+    public void setAdminNumber(int adminNumber) { 
+        this.adminNumber = adminNumber;
+    }
+
+    p
 }
